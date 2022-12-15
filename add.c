881905fda0338017fee0b58fc2ef9d49aaf68679
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-15 06:12:51 -0800

    monty

---
+++ add.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+* add - This function adds the top two nodes of the stack, stores it in
+* the second node, and pops off the top node
+* @stack: The stack list
+* @line_number: The line number of the file
+* Return: The edited stack
+*/
+
+stack_t *add(stack_t **stack, unsigned int line_number)
+{
+int sum = 0;
+stack_t *head = *stack;
+/* Check if stack is NULL */
+if (!head || !head->next)
+{
+/* Print error, free, and exit */
+fprintf(stderr, "L%d: can't add, stack too short\n",
+line_number);
+free_stack(stack);
+exit(EXIT_FAILURE);
+}
+/* Sum first two nodes, and store it in second node */
+sum = (head->n) + (head->next->n);
+head->next->n = sum;
+pop(stack, 0);
+return (*stack);
+}
