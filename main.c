8a7eeccc2c0918d6f82239d6b79dea974f99c16c
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-16 06:23:52 -0800

    monty

--- main.c
+++ main.c
@@ -12,168 +12,73 @@ int token = 1;
 */
 
 int main(int argc, char **argv)
-
 {
-
-		const char *filename;
-
-			char *string = NULL, *opcode, *num_str;
-
-				size_t nbytes = 1;
-
-					FILE *file;
-
-						unsigned int line_num = 0, i = 0;
-
-							ssize_t read_c = 0;
-
-								stack_t *stack;
-
-
-
-									stack = NULL;
-
-										if (argc != 2)
-
-												{
-
-															fprintf(stderr, "USAGE: monty file\n");
-
-																	exit(EXIT_FAILURE);
-
-																		}
-
-
-
-											filename = argv[1];
-
-
-
-												/* Open File with the bytecodes */
-
-												file = fopen(filename, "r");
-
-													if (file == NULL)
-
-															{
-
-																		fprintf(stderr, "Error: Can't open file %s\n", filename);
-
-																				exit(EXIT_FAILURE);
-
-																					}
-
-
-
-														/* Read file line by line */
-
-														while (read_c != EOF)
-
-																{
-
-																			token = 1;
-
-																					i = 0;
-
-																							if (string != NULL)
-
-																											free(string);
-
-																									string = NULL;
-
-
-
-																											read_c = getline(&string, &nbytes, file);
-
-
-
-																													if (read_c == -1)
-
-																																{
-
-																																				free(string);
-
-																																							if (stack != NULL)
-
-																																												free_stack(&stack);
-
-																																										fclose(file);
-
-																																													return (0);
-
-																																															}
-
-
-
-																															/* Keep count of the number of lines */
-
-																															line_num++;
-
-
-
-																																	/* Continue if line or string is NULL */
-
-																																	if (read_c == 0)
-
-																																					continue;
-
-
-
-																																			/* Continue if line had only the new line character */
-
-																																			if (read_c == 1)
-
-																																							continue;
-
-
-
-																																					/* Parse the first elements of the line */
-
-																																					opcode = strtok(string, " \n");
-
-
-
-																																							/* If string is empty, let's continue */
-
-																																							if (opcode == NULL)
-
-																																											continue;
-
-
-
-																																									if (opcode[0] == '#')
-
-																																												{
-
-																																																nop(&stack, line_num);
-
-																																																			continue;
-
-																																																					}
-
-
-
-																																											/* Check whether the first token is the opcode 'push' */
-
-																																											if (strcmp(opcode, "push") == 0)
-
-																																														{
-
-																																																		num_str = strtok(NULL, " \n");
-
-
-
-																																																					/* Check if token is a digit or NULL */
-
-																																																					if (num_str == NULL)
-
-																																																									{
-
-																																																														fprintf(stderr, "L%d: usage: push integer\n",
-
-																																																																					line_num);
-
-																																																																		free(string);
+const char *filename;
+char *string = NULL, *opcode, *num_str;
+size_t nbytes = 1;
+FILE *file;
+unsigned int line_num = 0, i = 0;
+ssize_t read_c = 0;
+stack_t *stack;
+stack = NULL;
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+filename = argv[1];
+/* Open File with the bytecodes */
+file = fopen(filename, "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
+}
+/* Read file line by line */
+while (read_c != EOF)
+{
+token = 1;
+i = 0;
+if (string != NULL)
+free(string);
+string = NULL;
+read_c = getline(&string, &nbytes, file);
+if (read_c == -1)
+{
+free(string);
+if (stack != NULL)
+free_stack(&stack);
+fclose(file);
+return (0);
+}
+/* Keep count of the number of lines */
+line_num++;
+/* Continue if line or string is NULL */
+if (read_c == 0)
+continue;
+/* Continue if line had only the new line character */
+if (read_c == 1)
+continue;
+/* Parse the first elements of the line */
+opcode = strtok(string, " \n");
+/* If string is empty, let's continue */
+if (opcode == NULL)
+continue;
+if (opcode[0] == '#')
+{
+nop(&stack, line_num);
+continue;
+}
+/* Check whether the first token is the opcode 'push' */
+if (strcmp(opcode, "push") == 0)
+{
+num_str = strtok(NULL, " \n");
+/* Check if token is a digit or NULL */
+if (num_str == NULL)
+{
+fprintf(stderr, "L%d: usage: push integer\n",
+line_num);
+free(string);
 
 																																																																						free_stack(&stack);
 
